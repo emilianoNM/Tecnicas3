aeb57b50dee7467066476d28f4ac3b1412457603
Francisco54321 <42306539+Francisco54321@users.noreply.github.com>
2018-08-19 23:26:36 -0500

    Add files via upload

---
+++ palindromo MezatorresFrancisco.py
@@ -0,0 +1,21 @@
+
+palindromos = [
+    
+	'anita lava la tina'
+]
+
+
+def es_palindromo(p):
+    
+	estandar = str(p).lower().replace(' ','')
+    
+	return estandar == estandar[::-1]
+    
+
+if _name_ == '_main_':
+   
+	for p in palindromos:
+        
+		resultado = es_palindromo(p)
+    
+	print(resultado)
