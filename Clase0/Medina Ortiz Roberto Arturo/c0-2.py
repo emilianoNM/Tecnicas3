7ebcd5f29f7d98087ea649f54b9faa8b80f2adc0
robertnidem <robertmaul@live.com.mx>
2018-08-21 22:51:18 -0500

    Add files via upload

---
+++ Clase0/Medina Ortiz Roberto Arturo/c0-2.py
@@ -0,0 +1,6 @@
+#2.30
+x=input("Ingresa el numero")
+
+len(x)
+for y in range (0,len(x)):
+  print (x[y])
---
+++ Clase0/Medina Ortiz Roberto Arturo/c0-3.py
@@ -0,0 +1,8 @@
+#2.16
+x=int(input("Ingresa un numero"))
+y=int(input("Ingresa otro numero"))
+print("Suma: ",x+y)
+print("Producto ",x*y)
+print("Diferencia: ",x-y)
+print("Cociente: ",x/y)
+print("Modulo: ",x%y)
