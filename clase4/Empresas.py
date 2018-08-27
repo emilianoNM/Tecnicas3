7dca214b42a5d0bb105820d4f174a1f3739c6a82
enava <enava@asimov.mx>
2018-08-27 07:47:54 -0500

    Ejemplo de clases

---
+++ clase4/Empresas.py
@@ -0,0 +1,9 @@
+#Clase de Empresa
+
+class UnidadEconomica(object):
+	"""docstring for ClassName"""
+	Length=0
+	def __init__(self,tipo,nombre,razonSocial):
+		self.nombre = nombre
+		self.tipo= tipo
+		self.razonSocial=razonSocial
