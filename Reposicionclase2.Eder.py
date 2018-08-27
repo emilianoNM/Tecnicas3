d832ed442962572b1d77cad16a3bd5933da6cac9
Eder96 <42273380+Eder96@users.noreply.github.com>
2018-08-27 08:04:58 -0500

    Add files via upload

---
+++ Reposicionclase2.Eder.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+num=int(raw_input("ESCRIBE UN NUMERO:"))
+factorial=1
+for i in range(1,(num+1)):
+     
+        factorial=factorial*i
+
+
+print("El factorial de %s es %s", num,factorial)
+
