ba6b265dde1f865cf1149911a8911a741b0a8dfa
Eder96 <42273380+Eder96@users.noreply.github.com>
2018-08-29 07:39:03 -0500

    Add files via upload

---
+++ Reposición220818EderGonzalez/Programa 1..py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#Carina Chavez Granados Programa para convertir sistema ingles a decimal
+
+def main():
+print "Convierte medidas inglesas a sistema metrico"
+millas = input("Ingrese las  millas ")
+pies = input(" Ingrese cuantos pies: ")
+pulgadas = input("Ingrese el numero de pulgadas: ")
+
+metros = 1609.344 * millas + 0.3048 * pies + 0.0254 * pulgadas
+print "La longitud es de ", metros, " metros"
+main()
+
