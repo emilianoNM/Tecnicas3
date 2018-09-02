cf3e16973f06124dc45db074fb3ca5fabc0dd035
klbfs <friendphoebe@hotmail.es>
2018-09-02 11:11:28 -0500

    Add files via upload

---
+++ practica2.1.py
@@ -0,0 +1,25 @@
+class Perros(object):
+ def __init__(self, raza, color, edad):
+        
+    self.raza=raza
+    self.color=color
+    self.edad=edad
+
+def getRaza(self):
+    
+    return self.raza
+def getColor(self):
+        
+        return self.color
+def getEdad(self):
+        return self.edad
+def setRaza(self, raza):
+        self.raza=raza
+
+def setColor(self, color):
+        self.color=color
+
+def setEdad(self, edad):
+        self.edad=edad
+
+print(raaza.raza)
