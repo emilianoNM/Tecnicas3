b35d55e75ee21a1b57ffa30b897d0d5933309cf2
klbfs <friendphoebe@hotmail.es>
2018-09-02 10:45:03 -0500

    Add files via upload

---
+++ Clases.py
@@ -0,0 +1,134 @@
+
+
+
+
+class Agricultura(object):
+    def _init_(clasedeactividad, nombre,razonsocial, correoe, paginadeinternet, tipodeestablecimiento,  telefono,ide, cd, numempleados):
+            self.clasedeactividad=clasedeactividad
+            self.nombre=nombre
+            self.razonsocial=razonsocial
+            self.correoe=correoe
+            self.paginadeinternet=paginadeinternet
+            self.tipodeestablecimiento=tipodeestablecimiento
+            self.telefono=telefono
+            self.ide=ide
+            self.cd=cd
+            self.numempleados=numempleados
+            
+class Mineria(object):
+    def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad,tipounidadeconomica,fechaincorporacion):
+        self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.tipounidadeconomica=tipounidadeconomica
+        self.fechaincorporacion=fechaincorporacion
+        
+
+class construccion(object):
+       def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+        self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+        
+class serviciostecnologicosytecnicos(object):
+                   def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+        sself.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+        
+class corporativos(object):
+            def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+        self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+        
+        
+class serviciosdesalud(object):
+         def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+        self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+        
+
+class transporte(object):
+         def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+       self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+class educacion(object):
+         def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+       self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+class deporteycultura(object):
+         def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+        self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
+        
+class inmobiliario(object):
+         def _init_(nombre,ide,correoe,paginainternet,telefono,ciudad,numempleados,actividad
+        self.nombre=nombre
+        self.ide=ide
+        self.correoe=correoe
+        self.paginadeinternet=paginadeinternet
+        self.telefono=telefono
+        self.ciudad=ciudad
+        self.numempleados=numempleados
+        self.actividad=actividad
+        self.
+        self.
