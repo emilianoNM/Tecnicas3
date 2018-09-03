408c1f459c0c2e75ccc8eff5bef6ee13130856d9
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-03 02:54:56 +0000

    Add files via upload

---
+++ clase 4 CGC/ServiciosEducacion.py
@@ -0,0 +1,15 @@
+class ServiciosEducacion(object):
+	Length=0
+	def __init__(self,nombreInstitucion,actividadEducativa,modalidad,nivelEducativo,fuentedeFinanciamiento,servicios,establecimiento,telefono,afiliaciones,paginaWeb):
+		self.nombreInstitucion= nombreInstitucion
+		self.actividadEducativa= actividadEducativa
+		self.modalidad= modalidad
+		self.nivelEducativo= nivelEducativo
+		self.fuentedeFinanciamiento = fuentedeFinanciamiento
+		self.servicios= servicios
+		self.establecimiento= establecimiento
+		self.telefono= telefono
+		self.afiliaciones= afiliaciones
+		self.paginaWeb= paginaWeb
+
+
