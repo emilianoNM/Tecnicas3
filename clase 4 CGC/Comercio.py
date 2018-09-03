097f368a4c28ab46401f4ffd4aef4a067a1d5011
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-03 01:58:54 +0000

    Add files via upload

---
+++ clase 4 CGC/Comercio.py
@@ -0,0 +1,14 @@
+class Comercio(object):
+	Length=0
+	def __init__(self,nombreProducto,nombreProvedor,nombreComerciante,establecimiento,mediodeDistribucion,numIdenFiscal,sectordeComercio,superficiedeVenta,telefono,telefono):
+		self.nombreProducto= nombreProducto
+		self.nombreProvedor= nombreProvedor
+		self.nombreComerciante = nombreComerciante
+		self.establecimiento= establecimiento
+		self.mediodeDistribucion= mediodeDistribucion
+		self.numIdenFiscal= numIdenFiscal
+		self.sectordeComercio= sectordeComercio
+		self.superficiedeVenta= superficiedeVenta
+		self.telefono= telefono
+		self.email= email
+
