a40714bd47e59b73412c769fea99cf6fe9cdc17f
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-03 03:51:13 +0000

    Add files via upload

---
+++ clase 4 CGC/ServiciosFinancieros.py
@@ -0,0 +1,15 @@
+class ServiciosFinancieros(object):
+	Length=0
+	def __init__(self,nombreInstitucion,serviciosdeBanca,tasadeInteres,sistemasdePago,sucursales,credito,sector,afiliaciones,iD,paginaWeb)
+		self.nombreInstitucion= nombreInstitucion
+		self.serviciosdeBanca= serviciosdeBanca
+		self.tasadeInteres= tasadeInteres
+		self.sistemasdePago = sistemasdePago
+		self.sucursales= sucursales
+		self.credito= credito
+		self.sector= sector
+		self.afiliaciones= afiliaciones
+		self.iD= iD
+		self.paginaWeb= telefono
+
+
