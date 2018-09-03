4a451630ec801e58c8b25ec526d679b766dbb21b
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-03 02:35:09 +0000

    Add files via upload

---
+++ clase 4 CGC/ServiciosSalud.py
@@ -0,0 +1,15 @@
+class ServiciosSalud(object):
+	Length=0
+	def __init__(self,nombreInstitucion,sector,fuentedeFinanciamiento,serviciosMedicos,especializacion,nivel,establecimientos,telefono,cantidadEmpleados,paginaWeb):
+		self.nombreInstitucion= nombreInstitucion
+		self.sector= sector
+		self.fuentedeFinanciamiento = fuentedeFinanciamiento
+		self.serviciosMedicos= serviciosMedicos
+		self.especializacion= especializacion
+		self.nivel= nivel
+		self.establecimientos= establecimientos
+		self.telefono= telefono
+		self.cantidadEmpleados= cantidadEmpleados
+		self.paginaWeb= paginaWeb
+
+
