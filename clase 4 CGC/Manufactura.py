75dbdadc59329d672ef3d79fcfdef6bdb851a141
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-03 03:23:24 +0000

    Add files via upload

---
+++ clase 4 CGC/Manufactura.py
@@ -0,0 +1,15 @@
+class Manufactura(object):
+	Length=0
+	def __init__(self,nombreEmpresa,procesoManufacturero,productosFabricados,tipomateriales,clasedeActividad,numeroEmpleados,tipodeUnidadEconómica,iD,direccion,telefono)
+		self.nombreEmpresa= nombreEmpresa
+		self.procesoManufacturero= procesoManufacturero
+		self.productosFabricados= productosFabricados
+		self.tipomateriales = tipomateriales
+		self.clasedeActividad= clasedeActividad
+		self.numeroEmpleados= numeroEmpleados
+		self.tipodeUnidadEconómica= tipodeUnidadEconómica
+		self.iD= iD
+		self.direccion= direccion
+		self.telefono= telefono
+
+
