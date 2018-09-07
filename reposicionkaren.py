25627a46908edbd355a3a08b60beb4e5d012c3fa
klbfs <friendphoebe@hotmail.es>
2018-09-07 17:59:11 -0500

    Update and rename añobkaren.py to reposicionkaren.py

---
+++ reposicionkaren.py
@@ -0,0 +1,16 @@
+#karen
+ano = int(input("introduce año"))
+
+
+
+if (ano % 4) == 0:
+   if (ano % 100) == 0:
+       if (ano % 400) == 0:
+           print("{0} es año bisiesto".format(ano))
+       else:
+           print("{0} no es año bisiesto".format(ano))
+   else:
+       print("{0} es año bisiesto".format(ano))
+else:
+   print("{0} no es año bisiesto".format(an2018
+                                         o))
