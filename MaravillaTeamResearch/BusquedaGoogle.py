6c358f1e18b57c52a8fe540ab99f78d747285d0e
Yocoyani Perez <yocoyaniperez1@gmail.com>
2018-09-10 07:27:50 -0500

    Programa para realizar busqueda, presenta un error 404 en la busqueda

---
+++ MaravillaTeamResearch/BusquedaGoogle.py
@@ -0,0 +1,14 @@
+from googleapiclient.discovery import build
+import pprint
+
+my_api_key = " AIzaSyDRmKBJOt8KP15ius8Do9jZZ6DAWLaJzmM "
+my_cse_id = " 001185345237879240366:rlpjdzpxkby "
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
+
+results = google_search('stackoverflow site:en.wikipedia.org', my_api_key, my_cse_id, num=10)
+for result in results:
+    pprint.pprint(result)
