3657a5205c42135787abb2c2951c2ace9c4eff8d
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-10 04:38:39 +0000

    Add files via upload

---
+++ Reposicion07092018CGC/fibonacci.py
@@ -0,0 +1,16 @@
+#Programa Chavez Granados Carina imprime serie de Fibonacci
+
+def fibonacci(n):
+    valoractual = 1
+    valoranterior = 1
+    for i in range(n - 1):
+        k = valoranterior
+        valoranterior = valoractual + valoranterior
+        valoractual = k
+    return valoractual
+    
+def fibonacci_r(n):
+ if n == 1 or n == 2:
+  return 1
+ return fibonacci_r(n - 1) + fibonacci_r(n - 2)
+
