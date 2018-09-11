f2969eb36dac03d1fe9f1eb32955d395c04b162e
yisuspeace <42346083+yisuspeace@users.noreply.github.com>
2018-09-11 00:05:00 -0500

    PACHECO RIOS JESUS

---
+++ clase 4/empresa.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import Empresas
+testEmpresa=Empresas.UnidadEconomica('CblueCo','Tecnologica','CblueCo Ingenieria SA De CV')
+
+
+# In[ ]:
+
+
+if(testEmpresa.nombre=='CblueCo'):
+    print("test de manera correcta")
+else():
+    print("test de nombre error")
+
+print(testEmpresa.tipo)
+print(testEmpresa.razonSocial)
