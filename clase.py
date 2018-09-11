e121c3f74cee8ef9646968affd1ae0291caaa2a3
yisuspeace <42346083+yisuspeace@users.noreply.github.com>
2018-09-11 00:10:03 -0500

    PACHECO RIOS JESUS

---
+++ clase.py
@@ -0,0 +1,5 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> class ActividadEconomica(object):
+    def __init__(self, Actividad):
+        self.Actividad=Actividad
---
+++ foda.py
@@ -0,0 +1,37 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+class FODA(object):
+    def __init__(self):
+        pass
+
+class ActaConstitutiva(object):
+    def __init__(self):
+        pass
+
+        #Si regitrada ante hacianda
+
+
+class MisicnVision(object):
+    def __init__(self):
+        pass
+
+
+class DeclaracionHacienda(object):
+    def __init__(self):
+        pass
+
+
+class Permisos(object):
+    def __init__(self):
+        pass
+
+
+class IMSS(object):
+    def __init__(self):
+        #Si sus empleados estan regitrados
+        pass
+
+
+class Deudas(object):
+    def __init__(self, Cantidad, Calidad):
+        self.Calidad=Calidad #Concurso de acredares, Reconocimeto de Deuda, Suspension de Pago
