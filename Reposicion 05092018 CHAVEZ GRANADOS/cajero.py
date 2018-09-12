e85b9dec7ebfcdc60e114ff13cd180269ac3d5a2
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-12 02:39:23 +0000

    Add files via upload

---
+++ Reposicion 05092018 CHAVEZ GRANADOS/cajero.py
@@ -0,0 +1,20 @@
+#Programa que trabaja como un cajero electronico
+Saldo=0
+operacion="si";
+while (operacion=="si"):
+	operacion = raw_input("INGRESE: deposito O retiro SEGUN LA OPERACION QUE DESEE REALIZAR")
+	if (operacion == "retiro"):
+		cantidad = input("Ingrese la cantidad que desea retirar de su cuenta: $")
+		if (cantidad <= Saldo):
+			Saldo = Saldo - cantidad
+			print "Se realizo de manera exitosa el retiro. su saldo es: $",Saldo
+		else:
+			print "No tiene cantidad suficiente para retirar, vuelva a intentarlo."
+	if (operacion == "deposito"):
+		deposito = input("Ingrese la cantidad a depositar: $")
+		Saldo = Saldo + deposito
+		print "Se realizo deposito, en su cuenta tiene disponible ahora: $",Saldo
+	operacion = raw_input("Desea realizar otra operacion? si/no: ")
+print "Saldo total: $",Saldo
+
+
