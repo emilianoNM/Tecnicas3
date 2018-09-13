3fd6ae229d8fb8ccb79cf208fe29a265908eb4db
OmarRdzM <42226443+OmarRdzM@users.noreply.github.com>
2018-09-13 05:59:20 +0000

    Rodríguez Miranda Omar

---
+++ 20Clases.py
@@ -0,0 +1,146 @@
+class zona(object):
+    def _init_(self,ZonaRural,ZonaConurbada,TipoAsentamiento)
+        self.ZonaRural= ZonaRural
+        self.ZonaConurbada= ZonaConurbada
+        self.TipoAsentamiento=TipoAsentamiento
+
+class DatosBasicos(object):
+    def _init_(self,nombre,telefono,direccion,correoelectronico,paginaInternet,redesSociales)
+        self.nombre=nombre
+        self.telefono=telefono
+        self.direccion=direccion
+        self.correoelectronico=correoelectronico
+        self.paginaInternet=paginaInternet
+        self.redesSociales=redesSociales
+
+class empleados(object):
+    def _init_(self,totalEmpleados,empleados,empleadas,otros)
+        self.totalEmpleados=totalEmpleados
+        self.empleados=empleados
+        self.empleadas=empleadas
+
+class pago(object):
+    def _init_(self,cuentaBancaria,monto,deducible,balance)
+        self.cuentaBancaria=ceuntaBancaria
+        self.monto=monto
+        self.deducible=deducible
+        self.balance=balance
+
+class servicio(object):
+    def _init_(self,tipoServicio,costo,duracion,escalafuncional)
+        self.tipoServicio=tipoServicio
+        self.costo=costo
+        self.duracion=duracion
+        escalafuncional=escalafuncional
+
+class producto(object):
+    def _init_(self,calidad,precio,cantidad,garantia)
+        self.calidad=calidad
+        self.precio=precio
+        self.cantidad=cantidad
+        self.garaantia=garantia
+
+class mercadoBursatil(object):
+    def _init_(self,cotizacionBolsa,acciones,propiedades,puntosporcentuales)
+        self.cotizacionBolsa=cotizacionBolsa
+        self.acciones=acciones
+        self.propiedades=propiedades
+        self.puntosporcentuales=puntosporcetuales
+
+class transporte(object):
+    def _init_(self,tipotransporte,unidades,horarios,capacidad)}
+        self.tipotransporte=tipotransporte
+        self.unidades=unidades
+        self.horarios=horarios
+        self.capacidad=capacidad
+
+class moneda(object):
+    def _init_(self,tipoMoneda,tipoCambio)
+        self.tipoMoneda=tipoMoneda
+        self.tipoCambio=tipoCambio
+
+class tiempoactiva(object):
+    def _init_(self,tiempo,fechainicio,fechaactual,continuidad)
+        self.tiempo=tiempo
+        self.fechainicio=fechainicio
+        self.fechaactual=fechaactual
+        self.continuidad=continuidad
+
+class importaciones(object):
+    def _init_(self,lote,origen,desitino,modoTraslado,tiempoTraslado)
+        self.lote=lote
+        self.origen=origen
+        self.destino=destino
+        self.modoTraslado=modoTraslado
+        self.tiempoTraslado=tiempoTraslado
+
+class sociedades(object):
+    def _init_(self,numeroSocios,socioMayoritario,socioMinoritario,TipoSocio)
+        self.numeroSocios=numeroSocios
+        self.socioMayoritario=socioMayoritario
+        self.socioMinoritario=socioMinoritario
+        self.TipoSocio=TipoSocio
+
+class transnacional(object):
+    def _init_(self,sede,totalPaises,totalContinentes,monedaTransacciones,tipoCambio,sucursales,acuerdosComerciales)
+        self.sede=sede
+        self.totalPaises=totalPaises
+        self.totalContinentes=totalContinentes
+        self.monedaTransacciones=monedaTransacciones
+        self.acerdosComerciales=acuerdosComerciales
+        self.tipoCambio=tipoCambio
+        self.sucursales=sucursales
+
+
+class nacional(object):
+        def _init_(self,sede,estados,sucursales)
+            self.sede=sede
+            self.estados=estados
+            self.sucursales=sucursales
+
+class marketing(object):
+    def _init_(self,mediosUso,costo,eficiencia,balanceEconomico)
+        self.mediosUso=mediosUso
+        self.costo=costo
+        self.eficiencia=eficiencia
+        self.balanceEconomico=balanceEconomico
+
+class vacantes(object):
+    def _init_(self,puesto,cantidadVacantes,salario,prestaciones,vacaciones,bono,requisitos)
+        self.puesto=puesto
+        self.cantidadVacantes=cantidadVacantes
+        self.salario=salario
+        self.prestaciones=prestaciones
+        self.vacaciones=vacaciones
+        self.bono=bono
+        self.requisitos=requisitos
+
+class rankingEmpresa(object):
+    def _init_(self,posiciónEstatal,posicionNacional,posicionContinente,posicionMundo)
+        self.posicionEstatal=posicionEstatal
+        self.psicionNacional=posicionNacional
+        self.PosicionContinente=posicionContinente
+        self.posicionMundo=posicionMundo
+
+class seguridadLaboral(object):
+    def _init_(self,despidosAnuales,motivo,puesto,tiempoenPuesto)
+        self.despidosAnuales=despidosAnuales
+        self.motivo=motivo
+        self.puesto=puesto
+        self.tiempoenPuesto=tiempoenPuesto
+
+class calidadproductos(object):
+    def _init_(self,cumpleGarantia,defectos,reembolsosoAnuales,cumpleEspecificaciones)
+        self.cumpleGarantia=cumpleGarantia
+        self.reembolsosAnuales=reembolsosAnuales
+        self.cumpleEspecificaciones=cumpleEspecificaciones
+        
+class CalidadServicio(object):
+    def _init_(self,tiempoRespuesta,tiempoefectivo,eficiencia,eficacia)
+        self.tiempoRespuesta=tiempoRespuesta
+        self.tempoEfectivo=tiempoEfectivo
+        self.eficiencia=eficiencia
+        self.eficacia=eficacia
+        
+    
+    
