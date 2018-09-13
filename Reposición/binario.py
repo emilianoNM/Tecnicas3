5bf10911925f404a26298c33f06089897f510810
OmarRdzM <42226443+OmarRdzM@users.noreply.github.com>
2018-09-12 20:35:36 -0500

    Delete binario.py

--- Reposición/binario.py
+++
@@ -1,6 +0,0 @@
-def binarizar(decimal):
-    binario = ''
-    while decimal // 2 != 0:
-        binario = str(decimal % 2) + binario
-        decimal = decimal // 2
-    return str(decimal) + binario
