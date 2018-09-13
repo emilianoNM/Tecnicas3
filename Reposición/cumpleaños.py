f6dfe0cf90a75040584085e37895183e44d4ec5b
OmarRdzM <42226443+OmarRdzM@users.noreply.github.com>
2018-09-13 01:38:10 +0000

    Rodríguez Miranda Omar

---
+++ Reposición/cumpleaños.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+def main():
+    a_curso = input ("Ingresa el anio en curso: ")
+    for i  in range (3):
+        nombre = raw_input ("Nombre de la persona: ")
+        nacimiento = input ("Anio de nacimiento: ")
+        print  nombre, "cumple", (a_curso - nacimiento), "anios en el", a_curso
