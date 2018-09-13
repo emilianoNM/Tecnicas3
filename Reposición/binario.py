560dee760bd539c6a723c6c08d5194eb50f3d22a
OmarRdzM <42226443+OmarRdzM@users.noreply.github.com>
2018-09-12 21:19:01 -0500

    Update binario.py

--- Reposición/binario.py
+++ Reposición/binario.py
@@ -4,3 +4,5 @@ def binarizar(decimal):
         binario = str(decimal % 2) + binario
         decimal = decimal // 2
     return str(decimal) + binario
+numero = int(input('Introduce el número a convertir a binario: '))
+print(binarizar(numero))
