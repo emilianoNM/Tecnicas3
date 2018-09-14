3869df1d47f4f0c424f5dd77c83b397d27f0b320
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:53:03 -0500

    Add files via upload

---
+++ 20ClasesdeEmpresas/20Clases_CuevasLuis/Empleados.py
@@ -0,0 +1,10 @@
+#Informacion sobre empleados de la empresa
+
+class Empleados(object):
+	"""docstring for Empleados"""
+	def __init__(self, NumerodeEmpleados, Posicion, Experiencia, Salario):
+		
+		self.NumerodeEmpleados=NumerodeEmpleados
+		self.Posicion=Posicion
+		self.Experiencia=Experiencia
+		self.Salario=Salario
