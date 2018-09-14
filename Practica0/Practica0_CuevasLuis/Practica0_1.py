8a6fcea623331edf5cf1d92b5cea654ca5c66b68
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:56:00 -0500

    Add files via upload

---
+++ Practica0/Practica0_CuevasLuis/Practica0_1.py
@@ -0,0 +1,9 @@
+#Conversion de Grados Kelvin a Farenheit
+
+print("Este Programa convierte grados Kelvin a grados Farenheit")
+
+Kelvin=float(input("Ingrese los grados Kelvin a convertir: "))
+
+Farenheit= ((Kelvin-273.15)*1.8)+32
+
+print((Kelvin),'grados kelin son igual a ',(Farenheit), 'grados Farenheit')
