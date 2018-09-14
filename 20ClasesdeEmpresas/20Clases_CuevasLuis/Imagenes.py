bc04e457a515b4f12f4ef77b95a86c413f493ad7
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:53:23 -0500

    Add files via upload

---
+++ 20ClasesdeEmpresas/20Clases_CuevasLuis/Imagenes.py
@@ -0,0 +1,13 @@
+#Imagenes de la Empresa
+
+class Imagenes(object):
+	"""docstring for Imagenes"""
+	def __init__(self, Fotografias, Logo, Productos, Personal, Patrocinadores, Locacion, Instalaciones):
+		
+		self.Fotografias=Fotografias
+		self.Locacion=Locacion
+		self.Logo=Logo
+		self.Productos=Productos
+		self.Patrocinadores=Patrocinadores
+		self.Personal=Personal
+		self.Instalaciones=Instalaciones
