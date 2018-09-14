070cd7ed77ae9402ac12b137ac77738fa044b086
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:52:39 -0500

    Add files via upload

---
+++ 20ClasesdeEmpresas/20Clases_CuevasLuis/Departamentos.py
@@ -0,0 +1,13 @@
+#Departamentos dentro de la empresa
+
+class Departamentos(object):
+	"""docstring for Departamentos"""
+	def __init__(self, Ventas, Finanzas, Logistica, Administracion, Produccion, Mercadotecnia, RecursosHumanos):
+		
+		self.Ventas=Ventas
+		self.Finanzas=Finanzas
+		self.Logistica=Logistica
+		self.Administracion=Administracion
+		self.Produccion=Produccion
+		self.Mercadotecnia=Mercadotecnia
+		self.RecursosHumanos=RecursosHumanos
