5dde6e2d2f6e7b6c79cf07097664c2a5a17abb9d
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:52:16 -0500

    Add files via upload

---
+++ 20ClasesdeEmpresas/20Clases_CuevasLuis/Contacto.py
@@ -0,0 +1,10 @@
+#Formas de Contacto de la empresa
+
+class Contacto(object):
+	"""docstring for Contacto"""
+	def __init__(self, Telefono, Fax, CorreoElectronico, Whatsapp):
+		
+		self.Telefono = Telefono
+		self.Fax=Fax
+		self.CorreoElectronico=CorreoElectronico
+		self.Whatsapp=Whatsapp
