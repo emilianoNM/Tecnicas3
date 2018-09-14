380eb38f72a8739a9f954288ecec55ac10f50885
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:49:49 -0500

    Add files via upload

---
+++ 20CLASESEMPRESedergonzalez/20Clases_CuevasLuis/ActividadEconomica.py
@@ -0,0 +1,8 @@
+#Clase Actividad Economica 
+
+class ActividadEconomica(object):
+    def __init__(self, Actividad,Tipo, Servicios, Productos):
+		self.Actividad=Actividad
+		self.Tipo=Tipo
+		self.Servicios=Servicios
+		self.Productos=Productos
