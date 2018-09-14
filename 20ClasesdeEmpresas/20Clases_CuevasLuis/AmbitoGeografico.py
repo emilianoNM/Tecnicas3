482b835f9bda3eb6e860fdc773197959c58ef190
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:51:23 -0500

    Add files via upload

---
+++ 20ClasesdeEmpresas/20Clases_CuevasLuis/AmbitoGeografico.py
@@ -0,0 +1,10 @@
+#Ambito de Actuacion geodrafica de la empresa
+
+class AmbitoGeografico(object):
+	"""docstring for AmbitoFeografico"""
+	def __init__(self, Local, Nacional, Multinacional, Transnacional):
+		
+		self.Local=Local
+		self.Nacional=Nacional
+		self.Multinacional=Multinacional
+		self.Transnacional=Transnacional
