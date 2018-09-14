48c31318d235d5c673429fb222ed997943d5107f
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-09-14 05:51:48 -0500

    Add files via upload

---
+++ 20ClasesdeEmpresas/20Clases_CuevasLuis/Capital.py
@@ -0,0 +1,13 @@
+#Capital con el que cuenta la empresa
+
+class Capital(object):
+	"""docstring for Capital"""
+	def __init__(self, Presupuesto, Inversiones, Donaciones, Activos, Pasivos, Ganancias, Perdidas):
+		
+		self.Presupuesto=Presupuesto
+		self.Inversiones=Inversiones
+		self.Donaciones=Donaciones
+		self.Activos=Activos
+		self.Pasivos=Pasivos
+		self.Ganancias=Ganancias
+		self.Perdidas=Perdidas
