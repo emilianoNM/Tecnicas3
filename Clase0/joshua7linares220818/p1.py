ce37265a4679168a0102a08037e0874505e0a7b6
joshua7linares <42164091+joshua7linares@users.noreply.github.com>
2018-09-14 01:26:23 +0000

    Add files via upload

---
+++ Clase0/joshua7linares220818/p1.py
@@ -0,0 +1,11 @@
+
+print "Linares Alonso Joshua A."
+print "impresion de un rectangulo con asteriscos"
+
+m=int(input("numero de renglones "))
+n=int(input("numero de columnas "))
+
+for i in range(1,m+1):
+     for j in range(1,n+1):
+       print "*", 
+     print (" ")  
---
+++ Clase0/joshua7linares220818/p2.py
@@ -0,0 +1,23 @@
+print "Linares Alonso Joshua A."
+print "impresion del contorno de un rectangulo con asteriscos"
+m=int(input("numero de renglones "))
+n=int(input("numero de columnas "))
+
+for i in range(1,m+1):
+  if (i==1 or i==m):
+     for j in range(1,n+1):
+       print "*",    
+
+  if(i>1 and i!=m):
+      print "*",
+      for j in range (2,n+1):
+        if(j==n):
+          print "*",
+        else:
+          print " ",  
+
+
+
+  print (" ")  
+
+
