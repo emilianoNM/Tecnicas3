10e6ed6094860ce1aa5fd2025e06431dcf5a4412
Flaco2601 <42209483+Flaco2601@users.noreply.github.com>
2018-09-16 16:52:11 -0500

    programas asistencias

---
+++ Reposiciones/imparopar.py
@@ -0,0 +1,6 @@
+### pedir al usuario un numero e indicarle si es par o impar
+numero = input('Introduzca un numero: ')
+if numero%2 == 0:
+    print 'Este numero es par'
+else:
+    print 'Este numero es impar'
---
+++ Reposiciones/sumasconrange.py
@@ -0,0 +1,5 @@
+###Calcular e imprimir la suma de los numeros 1+2+3+4+5+...+50 con el comando range
+suma = range(1, 51)
+print sum(suma)
+ #con el comando sum se suma los numeros de una lista
+ 
