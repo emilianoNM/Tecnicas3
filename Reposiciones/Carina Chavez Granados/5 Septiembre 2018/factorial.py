025b7dabdc2dd3c30f0592b011429a5bdf17ba34
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-17 02:30:06 +0000

    Add files via upload

---
+++ Reposiciones/Carina Chavez Granados/5 Septiembre 2018/factorial.py
@@ -0,0 +1,10 @@
+#Chavez Granados Carina programa que calcula factorial
+def main():
+
+n=int(input(‘Ingresa un numero natural ’))
+i=2
+d=1
+	while i<=n:
+        d*=i
+         i+=1
+print('El factorial es:',d)
---
+++ Reposiciones/Carina Chavez Granados/5 Septiembre 2018/tablasmul.py
@@ -0,0 +1,7 @@
+#Chavez Granados Carina imprime tablas de multiplicar
+
+def main():
+print 'Tablas de multiplicar'
+num = input ('ingrese un numero del 1 al 11')
+    for i in range(0, 11):
+print (str(num) + " * " + str(i) + " = " + str(num*i))
