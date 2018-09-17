58a845676debe12396efb1b40d416faf324ec15a
robertnidem <robertmaul@live.com.mx>
2018-09-17 04:40:09 +0000

    Add files via upload

---
+++ Reposiciones/MedinaOrtizRobertoArturo/22-08/3-40.py
@@ -0,0 +1,8 @@
+for j in range(0,8):
+  for i in range(0,8):
+    
+    print("* ",end="")
+  
+  print(" ")
+  if j in range(0,8,2):
+      print(" ",end="")  
---
+++ Reposiciones/MedinaOrtizRobertoArturo/22-08/3-42.py
@@ -0,0 +1,5 @@
+pi=float(3.14159)
+r=float(input("Ingresa el radio: "))
+print("diametro: ",r*2)
+print("circunferencia: ",2*pi*r)
+print("area: ",pi*r*r)
