45c83fdf2c2db128a3453b5c9811f8df68055cdc
robertnidem <robertmaul@live.com.mx>
2018-09-17 04:38:52 +0000

    Add files via upload

---
+++ Reposiciones/MedinaOrtizRobertoArturo/15-08/c1.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+if x>y:
+  print(x, " Es mayor que ",y)
+elif x==y:
+  print("Los numeros son iguales")
+else:
+  print(y, " Es mayor que ",x)
