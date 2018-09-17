e0dfe0c8bcbc8114ec5b681fac62494503203c43
Francisco54321 <42306539+Francisco54321@users.noreply.github.com>
2018-09-17 03:38:37 -0500

    Add files via upload

---
+++ Practica1/Practica_1.py
@@ -0,0 +1,18 @@
+#Solucíon practica 1
+#Programacion estructurada
+
+x=miclase()
+class Fraccion:
+    def __init__(self,numerador,denominador):
+        self.num = numerador
+        self.den = denominador
+        def mostrar(self):
+     print(self.num,"/",self.den)
+      miF = Fraccion(3,5)
+      miF.mostrar()
+      print(miF)
+      print("hize", miF, " del pastel")
+      def __str__(self):
+      return str(self.num)+"/"+str(self.den)
+     miF.__str__()
+     str(miF)
