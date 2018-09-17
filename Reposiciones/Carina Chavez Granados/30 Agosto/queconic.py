0825953d80ec8f93de26d898741997ee04da8944
Mista44 <42320717+Mista44@users.noreply.github.com>
2018-09-17 05:11:54 +0000

    Add files via upload

---
+++ Reposiciones/Carina Chavez Granados/30 Agosto/queconic.py
@@ -0,0 +1,30 @@
+#Programa que te indica que clase de conica es 
+def main():
+
+print ("La conica tiene la forma de :Ax² + Bxy + Cy² + Dx + Ey + F = 0 ")
+
+A = input("Introduzca A:")
+B = input("Introduzca B:")
+C = input("Insertar C:")
+D = input("Introduzca D:")
+E = input("Introducir E:")
+F = input("Inserte F:")
+
+if (A == B == C == 0) or (B! = 0):
+        print ("Cónica no encontrada.")
+
+else:
+        (B == D == E == 0) & (F <0):
+                (A == C) & (A> 0):
+                        print ("Es una circunferencia.")
+
+                elif (A! = C) & (A> 0) &(C> 0):
+                        print ("Es una elipse.")
+
+                elif (A <0) or (C <0):
+                        print ("Es una hipérbola.")
+                
+
+if (a == B == E == F == 0) & (C! = 0) & (D! = 0)) or ((B == C == D == F == 0) & (D = (A! = 0) & (E! = 0)):
+                print ("Es una parábola.")
+main()
