4e9ef2ba110b40bfc0136227c5395730bdfbac00
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 21:09:26 -0600

    Update and rename Internet.py to PaginaOficial.py

---
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/PaginaOficial.py
@@ -0,0 +1,15 @@
+class PaginaOficial(object):
+    def __init__(self, url, Creacion, Expiracion, Imagen1, Imagen2, Creador, Actilizacion, Mision, Vision, Visitantes):
+        self.url=url
+        self.Creacion=Creacion
+        self.Expiracion=Expiracion
+        self.Imagen1=Imagen1
+        self.Imagen2=Imagen2
+        self.Creador=Creador
+        self.Actilizacion=Actilizacion
+        self.Mision=Mision
+        self.Vision=Vision
+        self.Visitantes=Visitantes
+        
+
+
