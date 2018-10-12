54fa2e1412fc5f6d2cdd9d391cd2b5fbf9ccfe1a
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 21:29:50 -0600

    Update Relaciones.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Relaciones.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Relaciones.py
@@ -1,7 +1,12 @@
 class Relaciones(object):
-    def __init__(self, Distribuidores,Clientes, Competencia, Sindicato, CamaraConsesionario, Inversores):
-        self.Distribuidores=Distribuidores
-        self.Clientes=Distribuidores
-        self.Competencia=Competencia
+    def __init__(self, Distribuidores1,Distribuidores2, Cliente1,Cliente2 Competencia1,Competencia2,Competencia3, Sindicato, CamaraConsesionario, Inversores, ):
+        self.Distribuidores1=Distribuidores1
+        self.Distribuidores2=Distribuidores2
+        self.Cliente1=Cliente1
+        self.Cliente2=Cliente2
+        self.Competencia1=Competencia1
         self.Sindicato=Sindicato
         self.CamaraConsesionario=CamaraConsesionario
+        self.Competencia2=Competencia2
+        self.Competencia3=Competencia3
+        self.Inversores=Inversores
