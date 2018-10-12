d3b39ed297b9723cee3b7b771c1f47dd57317e2e
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 23:36:46 -0600

    Update DatosGenerales.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/DatosGenerales.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/DatosGenerales.py
@@ -1,3 +1,13 @@
 class DatosGenerales:
-  def__init__():
+  def__init__(self, Nombre, FechaCreacion, Actividad, Ciudad, Salarios, Vacantes, Infraestructura, Sucursales,Logo, Lema):
+    self.Nombre=Nombre
+    self.FechaCreacion=FechaCreacion
+    self.Actividad=Actividad
+    self.Ciudad=Ciudad
+    self.Salarios=Salarios
+    self.Vacantes=Vacantes
+    self.Infraestructura=Infraestructura
+    self.Sucursales=Sucursales
+    self.Logo=Logo
+    self.Lema=Lema
     
