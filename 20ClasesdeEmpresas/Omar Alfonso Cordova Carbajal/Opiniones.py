99a54bfd5f98bffafdd720f947668dce21bb7160
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 23:17:07 -0600

    Update Opiniones.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Opiniones.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Opiniones.py
@@ -1,4 +1,13 @@
 class Opiniones():
-    def __init__(self, Opinion, Calificacion):
-        self.Opinion=Opinion
-        self.Calificacion=Calificacion
+    def __init__(self, Opinion1,Opinion2,Opinion3,Opinion4,Opinion5,Opinion6,Opinion7,Opinion8,Opinion9,Opinion10):
+        self.Opinion1=Opinion1
+        self.Opinion2=Opinion2
+        self.Opinion3=Opinion3
+        self.Opinion4=Opinion4
+        self.Opinion5=Opinion5
+        self.Opinion6=Opinion6
+        self.Opinion7=Opinion7
+        self.Opinion8=Opinion8
+        self.Opinion9=Opinion9
+        self.Opinion10=Opinion10
+        
