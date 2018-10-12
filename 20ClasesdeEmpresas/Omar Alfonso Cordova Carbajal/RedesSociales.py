b8f6651744d37137781882a3d6b2804d48bc40f1
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 20:53:08 -0600

    Create RedesSociales.py

---
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/RedesSociales.py
@@ -0,0 +1,10 @@
+class RedesSociales(object):
+    def __init__(self, PaginaOficial, Facebook, Twitter, LinkedIn, Youtube, Instagram , WhatsApp, Telefono):
+        self.PaginaOficial=PaginaOficial
+        self.Facebook=Facebook
+        self.Twitter=Twitter
+        self.LinkedIn=LinkedIn
+        self.Youtube=Youtube
+        self.Instagram=Instagram
+        self.WhatsApp=WhatsApp
+        self.Telefono=Telefono
