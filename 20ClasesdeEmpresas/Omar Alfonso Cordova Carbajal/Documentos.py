0b584612adc96b7f73b903d826630b84b062b0f4
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 21:22:46 -0600

    Update Documentos.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Documentos.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Documentos.py
@@ -1,37 +1,14 @@
-class FODA(object):
-    def __init__(self):
-        pass
-
-class ActaConstitutiva(object):
-    def __init__(self):
-        pass
-
-        #Si regitrada ante hacianda
-
-
-class MisicnVision(object):
-    def __init__(self):
-        pass
-
-
-class DeclaracionHacienda(object):
-    def __init__(self):
-        pass
-
-
-class Permisos(object):
-    def __init__(self):
-        pass
-
-
-class IMSS(object):
-    def __init__(self):
-        #Si sus empleados estan regitrados
-        pass
-
-
-class Deudas(object):
-    def __init__(self, Cantidad, Calidad):
-        self.Calidad=Calidad #Concurso de acredares, Reconocimeto de Deuda, Suspension de Pago
-
-
+class Documentos:
+    def __init__(FODA,ActaConstitutiva, MisionVision, DeclaracionHacienda, Permisos, IMSS, Deudas, SupscripcionCamaraComercio, DocumentoExtra1, DocumentoExtra2):
+        self.FODA=FODA
+        self.ActaConstitutiva=ActaConstitutiva
+        self.MisionVision=MisionVision
+        self.DeclaracionHacienda=DeclaracionHacienda
+        self.Permisos=Permisos
+        self.IMSS=IMSS
+        self.Deudas=Deudas
+        self.SupscripcionCamaraComercio=SupscripcionCamaraComercio
+        self.DocumentoExtra1=DocumentoExtra1
+        self.DocumentoExtra2=DocumentoExtra2
+        
+        
