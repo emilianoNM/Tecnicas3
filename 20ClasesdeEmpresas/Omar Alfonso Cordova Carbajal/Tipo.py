be7973846eb52227095e43d08acf6e2f0572f141
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 21:47:58 -0600

    Update Tipo.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Tipo.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Tipo.py
@@ -1,6 +1,13 @@
 class Tipo(object):
-    def __init__(self, Sociedad, Tamaño, Cuota, Ambito):
-        self.Sociedad=Sociedad
-        self.Tamaño=Tamaño
-        self.Cuota=Cuota
-        self.Ambito=Ambito
+    def __init__(self, Individual, ResponsabilidadLimitada, ComunidadBienes, SociedadCivil, SociedadColectiva, SociedadComandiariasimple, SociedadResponsabilidadLimitada, SociedadLimitadaFormaciónSucesiva, SociedadLimitadaNuevaEmpresa, SociedadAnónima):
+        self.Individual=Individual
+        self.ResponsabilidadLimitada=ResponsabilidadLimitada
+        self.ComunidadBienes=ComunidadBienes
+        self.SociedadCivil=SociedadCivil
+        self.SociedadColectiva=SociedadColectiva
+        self.SociedadComandiariasimple=SociedadComandiariasimple
+        self.SociedadResponsabilidadLimitada=SociedadResponsabilidadLimitada
+        self.SociedadLimitadaFormaciónSucesiva=SociedadLimitadaFormaciónSucesiva
+        self.SociedadLimitadaNuevaEmpresa=SociedadLimitadaNuevaEmpresa
+        self.SociedadAnónima=SociedadAnónima
+        
