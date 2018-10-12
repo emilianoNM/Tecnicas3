6a8f81ceb584822e820396b7f34db4f620d8661e
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 21:14:00 -0600

    Update Tamaño.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Tamaño.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Tamaño.py
@@ -1,8 +1,12 @@
 class Tamaño(object):
-    def __init__(self, Volumen_Ventas, Capital_Propio, Número_Trabajadores, Beneficios, Patentes, Sucursales):
+    def __init__(self, Volumen_Ventas, Capital_Propio, Número_Trabajadores, Beneficios, Patentes, Sucursales, Superficie, NumeroPropiedades, ValorTotal):
         self.Volumen_Ventas=Volumen_Ventas
         self.Capital_Propio=Capital_Propio
         self.Número_Trabajadores=Número_Trabajadores
         self.Beneficios=Beneficios
         self.Patentes=Patentes
         self.Sucursales=Sucursales
+        self.Superficie=Superficie
+        self.NumeroPropiedades=NumeroPropiedades
+        self.ValorTotal=ValorTotal
+        
