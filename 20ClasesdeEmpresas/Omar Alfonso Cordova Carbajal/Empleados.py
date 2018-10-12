15b5ee3ef0e977814295611527f2a52e90a87526
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 21:02:23 -0600

    Update and rename Empleados.py to Personal.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Empleados.py
+++
@@ -1,5 +0,0 @@
-class Personal(object):
-    def __init__(self, Confianza, Sindicalizados):
-        self.Confianza=Confianza
-        self.Sindicalizados=Sindicalizados
-
---
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Personal.py
@@ -0,0 +1,13 @@
+class Personal(object):
+    def __init__(self, Confianza, Sindicalizados, CantidadTotal, Nacionales, Internacionales, Despedidos, Tecnicos, Administrativos, Atencion, Investigacion):
+        self.Confianza=Confianza
+        self.Sindicalizados=Sindicalizados
+        self.CantidadTotal=CantidadTotal
+        self.Nacionales=Nacionales
+        self.Internacionales=Internacionales
+        self.Despedidos=Despedidos
+        self.Tecnicos=Tecnicos
+        self.Administrativos=Administrativos
+        self.Atencion=Atencion
+        self.Investigacion=Investigacion
+
