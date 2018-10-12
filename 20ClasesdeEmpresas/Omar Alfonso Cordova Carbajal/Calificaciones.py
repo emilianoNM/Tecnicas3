8e0d3da3599491507238ea334ec0bca86eeb6ad6
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 23:03:26 -0600

    Update Calificaciones.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Calificaciones.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Calificaciones.py
@@ -1,3 +1,13 @@
 class Calificaciones(object):
-    def __init__(self, Fuentes):
-        self.Fuentes=Fuentes
+    def __init__(self, Calif1, Calif2,Calif3,Calif4,Calif5,Calif6,Calif7,Calif8,Calif9,Calif10):
+        self.Calif1=Calif1
+        self.Calif2=Calif2
+        self.Calif3=Calif3
+        self.Calif4=Calif4
+        self.Calif5=Calif5
+        self.Calif6=Calif6
+        self.Calif7=Calif7
+        self.Calif8=Calif8
+        self.Calif9=Calif9
+        self.Calif10=Calif10
+
