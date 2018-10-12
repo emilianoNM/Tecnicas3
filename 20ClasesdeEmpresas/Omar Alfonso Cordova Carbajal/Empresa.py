c7b6d6615eb7b19e91544d54b7b85ce0e2461bbd
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 23:49:11 -0600

    Update Empresa.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Empresa.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Empresa.py
@@ -1,1 +1,43 @@
+from Documentos import Documentos
+from PaginaOficial import PaginaOficial
+from Personal import Personal
+from RedesSociales import RedesSociales
+from Relaciones import Relaciones
+from Tamaño import Tamaño
+from Tipo import Tipo
+from Calificaciones import Calificaciones
+from Opiniones import Opiniones
+from Imagenes import Imagenes
+from Posicion import Posicion
+from ActividadEconomica import 
+from Sucursal import 
+from DatosGenerales import DatosGenerales
+from  import 
+from  import 
+from  import 
+from  import 
+from  import 
+
 class Empresa:
+  def __init__():
+    self.E_Documentos
+    self.E_PaginaOficial
+    self.E_Personal
+    self.E_RedesSociales
+    self.E_Relaciones
+    self.E_Tamaño
+    self.E_Tipo
+    self.E_Calificaciones
+    self.E_Opiniones
+    self.E_Imagenes
+    self.E_Posicion
+    self.E_ActividadEconomica
+    self.E_Sucursal
+    self.E_DatosGenerales
+    self.E_
+    self.E_
+    self.E_
+    self.E_
+    self.E_
+
+    
