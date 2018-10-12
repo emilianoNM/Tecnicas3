11da13dd579a5e20e3e7c224004789ad6c1af764
Omar Alfonso Cordova <elsupermegareyesomar@gmail.com>
2018-10-11 23:22:08 -0600

    Update Imagenes.py

--- 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Imagenes.py
+++ 20ClasesdeEmpresas/Omar Alfonso Cordova Carbajal/Imagenes.py
@@ -1,4 +1,13 @@
-class Imegenes(object):
-    def __init__(self, Negocio, Producto):
-        self.Negocio=Negocio
-        self.Producto=Producto
+class Imagenes(object):
+    def __init__(self, Imagen1, Imagen2, Imagen3, Imagen4, Imagen5, Imagen6, Imagen7, Imagen8, Imagen9, Imagen10):
+        self.Imagen1=Imagen1
+        self.Imagen2=Imagen2
+        self.Imagen3=Imagen3
+        self.Imagen4=Imagen4
+        self.Imagen5=Imagen5
+        self.Imagen6=Imagen6
+        self.Imagen7=Imagen7
+        self.Imagen8=Imagen8
+        self.Imagen9=Imagen9
+        self.Imagen10=Imagen10
+        
